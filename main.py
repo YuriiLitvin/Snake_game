d74ae5cd0033d4df5082d299a29eaed497986322
yurijlitviniii@gmail.com <yurijlitviniii@gmail.com>
2022-06-17 02:13:13 +0300

    class Snake created constructor added move() method created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,33 +8,14 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-start_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in start_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    # new_segment.speed("slowest")
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-segments[0].color("red")
-segments[1].color("green")
-segments[2].color("blue")
-
-
-# def turn_left():
-#     segments[0].left(90)
+snake = Snake()
 
 
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-    for index in range(len(segments) - 1, 0, -1):
-        pos = segments[index - 1].position()
-        segments[index].goto(pos)
-    segments[0].forward(20)
+    snake.move()
 
 # screen.listen()
 # screen.onkey(turn_left, "a")
---
+++ snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+        self.segments[0].color("red")
+        self.segments[1].color("green")
+        self.segments[2].color("blue")
+
+    def move(self):
+        for index in range(len(self.segments) - 1, 0, -1):
+            pos = self.segments[index - 1].position()
+            self.segments[index].goto(pos)
+        self.segments[0].forward(20)
