1a922d8bc7010dfae13f94834e86ac6cfa804e4a
yurijlitviniii@gmail.com <yurijlitviniii@gmail.com>
2022-06-17 22:37:16 +0300

    class Scoreboard: attributes added method count() added Score increases by 1 every time the snake "eats" the point

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+Y_POSITION = 270
 
 
 class Scoreboard(Turtle):
@@ -6,3 +7,15 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.penup()
+        self.color("white")
+        self.ht()
+        self.sety(Y_POSITION)
+        self.count()
+
+    def count(self):
+        self.clear()
+        self.write(f"SCORE: {self.score}", False, align="center", font=('Arial', 16, 'normal'))
+        self.score += 1
+
+
